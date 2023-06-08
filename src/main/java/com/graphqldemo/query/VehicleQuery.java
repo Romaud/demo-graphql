936229421f62fb0f6a3e693e8ab22e0cf6c09b9e
Roman Vankov <ramoon.doc@gmail.com>
2023-06-09 00:46:40 +0300

    подправил id
    изменил логику Id и добавл string

--- src/main/java/com/graphqldemo/query/VehicleQuery.java
+++ src/main/java/com/graphqldemo/query/VehicleQuery.java
@@ -25,6 +25,9 @@ public class VehicleQuery{
     @QueryMapping
     public Optional<Vehicle> getVehicle(@Argument int id) {
         System.out.println(id);
+        String s = "7";
+        id = 3;
+        String x = "x";
         System.out.println(id);
         System.out.println(id);
         return this.vehicleService.getVehicle(id);
