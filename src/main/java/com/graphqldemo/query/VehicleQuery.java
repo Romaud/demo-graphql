4cdb4c3979dbdadac3140c6b2f91345c41f560a2
Roman Vankov <ramoon.doc@gmail.com>
2025-02-11 10:20:10 +0300

    VehicleMutation

--- src/main/java/com/graphqldemo/query/VehicleQuery.java
+++ src/main/java/com/graphqldemo/query/VehicleQuery.java
@@ -28,10 +28,10 @@ public class VehicleQuery{
     public Optional<Vehicle> getVehicle(@Argument int id) {
         System.out.println(id);
         System.out.println(id);
+        System.out.println(id);
         String y = "7";
         id = 3;
         String x = "x";
-        System.out.println(id);
         System.out.println(id);
         return this.vehicleService.getVehicle(id);
     }
